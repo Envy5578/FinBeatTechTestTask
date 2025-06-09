4be8f1d63af518c21d74f4d2dedcae3f5cf2a72c
Зозук Андрей <and_r_ey@bk.ru>
2025-06-09 12:12:06 +0400

    refactor summary for swagger

--- FinBeatTechTestTask/Controllers/PairValueController.cs
+++ FinBeatTechTestTask/Controllers/PairValueController.cs
@@ -17,14 +17,26 @@ namespace FinBeatTechTestTask.Controllers
         }
 
         /// <summary>
-        /// Получение Json массива объектов
+        /// Updates pair values with the provided list
         /// </summary>
-        /// <param name="model">PairValueViewModel object</param>
-        /// <returns>
-        /// Returns an HTTP response with the result of the operation:
-        /// 200 OK if the update was successful, 400 Bad Request in case of an error.
-        /// The response body contains a description of the result.
-        /// </returns>
+        /// <remarks>
+        /// Sample request:
+        /// POST /pairValues/refresh
+        /// [
+        ///     {
+        ///         "key": 101,
+        ///         "value": "Some value A"
+        ///     },
+        ///     {
+        ///         "key": 202,
+        ///         "value": "Some value B"
+        ///     }
+        /// ]
+        /// </remarks>
+        /// <param name="model">List of PairValueViewModel with integer keys and string values</param>
+        /// <returns>Returns a description of the result of the operation</returns>
+        /// <response code="200">Update successful</response>
+        /// <response code="400">Invalid input or operation failed</response>
         [HttpPost]
         public async Task<IActionResult> RefreshPairValues(List<PairValueViewModel> model)
         {
@@ -39,10 +51,36 @@ namespace FinBeatTechTestTask.Controllers
         }
 
         /// <summary>
-        /// Отправка сортированных данных
+        /// Retrieves a filtered and paginated list of pair values
         /// </summary>
-        /// <param>PairValueFilter object</param>
-        /// <returns>Return PairValueViewModel</returns>
+        /// <remarks>
+        /// Sample request:
+        /// GET /pairValues?search=abc&pageSize=10&skip=0
+        /// 
+        /// Sample response:
+        /// {
+        ///     "data": [
+        ///         {
+        ///             "id": 1,
+        ///             "key": 101,
+        ///             "value": "Some value A"
+        ///         },
+        ///         {
+        ///             "id": 2,
+        ///             "key": 202,
+        ///             "value": "Some value B"
+        ///         }
+        ///     ],
+        ///     "total": 2,
+        ///     "pageSize": 10,
+        ///     "skip": 0
+        /// }
+        /// </remarks>
+        /// <param name="filter">Filter parameters for searching and pagination</param>
+        /// <returns>Returns a paginated list of PairValueViewModel objects</returns>
+        /// <response code="200">Successful retrieval</response>
+        /// <response code="404">No data found</response>
+        /// <response code="400">Invalid filter parameters</response>
         [HttpGet]
         public async Task<IActionResult> GetPairValues([FromQuery] PairValueFilter filter)
         {
